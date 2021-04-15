731576e8b4dfe6a8ed45a505317d5694f04f6f7c
Grayson-choi <784wldnd@gmail.com>
2021-04-15 22:53:47 +0900

    article model 작성

--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=20) # max_length는 필수 인자
+    content = models.TextField()
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"제목:{self.title}, 내용:{self.content}"
+
+    
