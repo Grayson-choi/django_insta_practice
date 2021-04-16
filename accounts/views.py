82bd9ee577999bf2789fdc2baae3d7cf62b36c13
Grayson-choi <784wldnd@gmail.com>
2021-04-16 13:02:15 +0900

    accounts CustomUserModel Signup 완성

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import CustomUserCreationForm
+from django.contrib.auth import login as auth_login
 
 # Create your views here.
 def index(request):
     return render(request, 'accounts/index.html')
+
+
+def signup(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('articles:index')
+    else:
+        form = CustomUserCreationForm()
+    context = {
+        'form': form,
+    }
+    return render(request, 'accounts/signup.html', context)
